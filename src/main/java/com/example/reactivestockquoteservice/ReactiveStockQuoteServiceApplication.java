ffd3897d42865f8da5a7211a48e4a26ebf0141ec
Max Faupel <max.faupel@gmx.de>
2021-03-21 12:25:22 +0100

    first commit

---
+++ src/main/java/com/example/reactivestockquoteservice/ReactiveStockQuoteServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactivestockquoteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveStockQuoteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveStockQuoteServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reactivestockquoteservice/ReactiveStockQuoteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactivestockquoteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveStockQuoteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
